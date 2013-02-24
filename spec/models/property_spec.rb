39562cafa110ec54f274f544c0f68b96f2ec3676
Jeremy Woertink <jeremywoertink@gmail.com>
2013-02-24 14:35:01 -0800

    doing some testing

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
-describe Property do
+describe TouRETS::Property do
   
-  describe '#all' do
+  context 'when searching for 3 bedrooms', vcr: { cassette_name: 'properties/3_bedrooms' } do
     
-    it 'returns all results' do
-      pending
+    subject { @properties.collect(&:bedrooms) }
+    
+    before do
+      @properties = TouRETS::Property.where(bedrooms: 3)
+    end
+    
+    after do
+      TouRETS.close_connection
+    end
+    
+    it 'returns properties with 3 bedrooms' do
+      expect(subject).to include(3)
+    end
+    
+    it 'excludes properties more than 3 bedrooms' do
+      expect(subject).to_not include(4)
+    end
+    
+    it 'excludes properties less than 3 bedrooms' do
+      expect(subject).to_not include(2)
     end
   end
   
